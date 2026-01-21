1f68a82da2205f279968bca12c518675df792caa
Diana <dianar11092006@gmail.com>
2026-01-21 17:21:10 -0500

    Create day2_variables.py

---
+++ Python practice/day2_variables.py
@@ -0,0 +1,8 @@
+name = input(“What is your name? “)
+age = int(input(“How old are you? “))
+
+year = 2026
+birth_year = year - age
+
+print(“Hello,”, name)
+print(“You were born around”, birth_year)
